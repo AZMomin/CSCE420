c98163291a9da9149d251155c29aa89f4a46c5b9
AZMomin <ali.shan1014@gmail.com>
2014-10-01 00:05:26 -0500

    Update BFS_main.cpp

--- BFS/BFS_main.cpp
+++ BFS/BFS_main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <math.h>   
 #include "Node.h"
 using namespace std;
 
@@ -10,34 +11,51 @@ using namespace std;
 int num_of_vertices, num_of_edges; 
 vector<pair<int,int> > vertices;
 vector<pair<int,int> > edges;
-int vertices_visited = 0;
+int input_vertex, goal_vertex;
+int max_frontier_size = 0;
+int vertices_visited = 1;
+// ------------------------ COMPARATOR CLASS -------------------------
+struct compareNodes {
+    bool operator()(const Node &n1, const Node &n2) {
+        return n1.heuristic < n2.heuristic;
+    }
+};
 // -------------------------------------------------------------------
-// 					TEMPORARY SUCCESSOR FUNCTION 
+// 							LOCAL FUNCTIONS
 // -------------------------------------------------------------------
+double set_heuristic(Node * node){
+	return sqrt(  (pow((double(vertices[node->v].first) - double(vertices[goal_vertex].first)),2)) +
+				  (pow((double(vertices[node->v].second) - double(vertices[goal_vertex].second)),2))
+				);
+}
+
 vector<Node * > successors(Node * parent){
 	vector<Node * > children;
 	for(int i = 0; i < num_of_edges; i++){
 		if (edges[i].first == parent -> v){
 			Node * temp = new Node(edges[i].second, parent);
 			temp -> depth = parent -> depth +1;
+			temp -> heuristic = set_heuristic(temp);
 			children.push_back(temp);
 		}
 		else if (edges[i].second ==  parent -> v){
 			Node * temp = new Node(edges[i].first, parent);
 			temp -> depth = parent -> depth +1;
+			temp -> heuristic = set_heuristic(temp);
 			children.push_back(temp);
 		}
 	}
 	return children;
 }
+
+
 // -------------------------------------------------------------------
 // 					BREADTH-FIRST SEARCH FUNCTION 
 // -------------------------------------------------------------------
 Node * Search(Node * initial_state, int goal){
 	queue <Node*>  frontier;
-	cout << "Size after creation: " << frontier.size() << endl;
-
 	vector<bool> visted;
+	
 	for(int i = 0; i < num_of_vertices; i++){
 		visted.push_back(false);
 	}
@@ -45,6 +63,8 @@ Node * Search(Node * initial_state, int goal){
 	frontier.push(initial_state);
 
 	while(!(frontier.empty())){
+		if (frontier.size() > max_frontier_size)
+			max_frontier_size = frontier.size();
 		Node * temp = frontier.front();
 		cout << "Checking Vertex:  " << temp -> v << endl;
 		frontier.pop();
@@ -52,6 +72,7 @@ Node * Search(Node * initial_state, int goal){
 			return temp; 
 		else if (visted[temp -> v] == false){
 			visted[temp -> v] = true;
+			vertices_visited++;
 			vector<Node * > children = successors(temp);
 			cout << "Pushing child vertices: ";
 			for (int i = 0; i < children.size(); i++){
@@ -74,7 +95,6 @@ int main(){
 	int index, element1, element2;			
 	string line;
 	string filename;
-	int input_vertex, goal_vertex; 
 
 	cout << "Enter file name!" << endl;
 	cin >> filename; 
@@ -127,23 +147,26 @@ int main(){
   	Node * initial = new Node(input_vertex, NULL);
   	vector<Node * > children = successors(initial);
 	initial -> successors = children;
+	initial -> heuristic = set_heuristic(initial);
 	for (int i = 0; i < initial -> successors.size(); i++){
 		cout << initial -> successors[i] -> v << endl; 
 	}
 	//-----------------   START SEARCH ON FIRST NODE ---------------------
 	Node * _goal = Search(initial, goal_vertex);
 
-	cout << "Goal: " << _goal -> v << endl << endl;
+	cout << "\nGoal: " << _goal -> v << endl;
 	vector<Node * > path = _goal -> traceback();
 	// -------------------------------------------------------------------
-	// 						PRINT OUT INITIAL DATA
+	// 						PRINT OUT DATA
 	// -------------------------------------------------------------------
 	cout << "Vertices: " << num_of_vertices << "\tEdges: " << num_of_edges << endl;
 	cout << "Start: (" << vertices[input_vertex].first << "," << vertices[input_vertex].second << ")\t";
 	cout << "Goal: (" << vertices[goal_vertex].first << "," << vertices[goal_vertex].second << ")\t";
 	cout << "Vertices: " << input_vertex << " and " << goal_vertex << endl;
-	//cout << "Vertices Visited: " << vertices_visited << "/" << num_of_vertices << endl;
+	cout << "Verticies Visited: " << vertices_visited << endl;
 	cout << "Path Length: " << path.size() << endl << endl;
+	cout << "Max queue size: " << max_frontier_size << endl;
+	cout << "Solution: " << endl;
 	// ------------------------- TRACEBACK ------------------------------- 
 	for(int i = path.size() -1 ; i >= 0; i--){
 		cout << "Vertex" << " " << path[i] -> v << "\t" ;
